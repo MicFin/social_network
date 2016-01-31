ec95c8dc6ffb7ee70d871825a13651a08a1f4f20
Michael Finneran <mrfinneran@gmail.com>
2016-01-31 13:11:44 -0500

    added gravatar to user and users show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
   devise_for :users, controllers: {registrations: 'registrations'}
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 
-  get 'pages/home'
+  # You can have the root of your site routed with "root"
   root 'pages#home'
+
+  resources :users, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
