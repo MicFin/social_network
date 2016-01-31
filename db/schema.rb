4336f7ea1ebcd421159e4bc7f3f519cf50a71aa0
Michael Finneran <mrfinneran@gmail.com>
2016-01-31 13:29:34 -0500

    Added Friendships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131181200) do
+ActiveRecord::Schema.define(version: 20160131182448) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.string   "state",       default: "pending"
+    t.datetime "friended_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
