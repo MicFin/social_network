e5a43901718cde5e0342eb6f9b3e1c89cc60123e
Michael Finneran <mrfinneran@gmail.com>
2016-01-31 13:22:32 -0500

    Added users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
-  before_action :set_user, only:[:show]
-  
-  def show
-  end
+    before_action :set_user, only:[:show]
+    
+    def index
+      @users = User.all
+    end
 
-  private
+    def show
+    end
 
-  def set_user
-    @user = User.find_by(username: params[:id])
-  end
-  
+    private
+
+    def set_user
+      @user = User.find_by(username: params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
